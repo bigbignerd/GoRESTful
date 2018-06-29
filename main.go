7a49cfa2e520f81845abcd59d30a1b5853621c76
wanguohui <wanguohui@yahoo.com>
2018-06-29 23:43:45 +0800

    add config loader

--- main.go
+++ main.go
@@ -4,10 +4,27 @@ import (
     "net/http"
     "log"
     "github.com/bigbignerd/GoRESTful/router"
+    "github.com/bigbignerd/GoRESTful/config"
+
     "github.com/gin-gonic/gin"
+    "github.com/spf13/pflag"
+    "github.com/spf13/viper"
 )
 
+var (
+    cfg = pflag.StringP("config", "c", "", "apisever cofig file path")
+)
 func main(){
+    pflag.Parse()
+
+	// init config
+	if err := config.Init(*cfg); err != nil {
+		panic(err)
+	}
+
+	// Set gin mode.
+	gin.SetMode(viper.GetString("runmode"))
+
     g := gin.New()
     middlewares := []gin.HandlerFunc{}
 
@@ -15,6 +32,6 @@ func main(){
         g,
         middlewares...,
     )
-    log.Print("start to listening the incoming requests on http address %s", ":8080")
-    log.Print(http.ListenAndServe(":8080", g).Error())
+    log.Printf("start to listening the incoming requests on http address %s",viper.GetString("addr")) 
+    log.Printf(http.ListenAndServe(viper.GetString("addr"), g).Error())
 }
--- router/middleware/header.go
+++ router/middleware/header.go
@@ -1,31 +1,31 @@
 package middleware
 
 import (
-    "net/http"
-    "time"
-    "github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"time"
 )
 
 //add header prevent client cache http response
 func NoCache(c *gin.Context) {
-    c.Header("Cache-Control", "no-cache, no-store, max-age=0,must-revalidate, value")
-    c.Header("Expires", "Thu,01 Jan 1970 00:00:00 GMT")
-    c.Header("Last-Modified", time.Now().UTC().Format(http.TimeFormat))
-    c.Next()
+	c.Header("Cache-Control", "no-cache, no-store, max-age=0,must-revalidate, value")
+	c.Header("Expires", "Thu,01 Jan 1970 00:00:00 GMT")
+	c.Header("Last-Modified", time.Now().UTC().Format(http.TimeFormat))
+	c.Next()
 }
 
 //CORS request header
-func Options(c *gin.Context){
-    if c.Request.Method != "OPTIONS" {
-        c.Next()
-    } else {
-        c.Header("Access-Control-Allow-Origin", "*")
-        c.Header("Access-Control-Allow-Methods", "GET,POST,PATCH,DELETE,OPTIONS")
-        c.Header("Access-Control-Allow-Headers", "authorization,origin,content-type,accept")
-        c.Header("Allow", "HEAD,GET,POST,PATCH,DELETE,OPTIONS")
-        c.Header("Content-Type", "application/json")
-        c.AbortWithStatus(200)
-    }
+func Options(c *gin.Context) {
+	if c.Request.Method != "OPTIONS" {
+		c.Next()
+	} else {
+		c.Header("Access-Control-Allow-Origin", "*")
+		c.Header("Access-Control-Allow-Methods", "GET,POST,PATCH,DELETE,OPTIONS")
+		c.Header("Access-Control-Allow-Headers", "authorization,origin,content-type,accept")
+		c.Header("Allow", "HEAD,GET,POST,PATCH,DELETE,OPTIONS")
+		c.Header("Content-Type", "application/json")
+		c.AbortWithStatus(200)
+	}
 }
 
 //secure header
