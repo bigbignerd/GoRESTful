81ca24f5b4068f7b0c613e1eee335b64f6b008d2
wanguohui <wanguohui@yahoo.com>
2018-07-02 23:33:57 +0800

    add mysql db connector

--- config/config.go
+++ config/config.go
@@ -2,8 +2,8 @@ package config
 
 import (
 	"github.com/fsnotify/fsnotify"
-	"github.com/spf13/viper"
 	"github.com/lexkong/log"
+	"github.com/spf13/viper"
 	"strings"
 )
 
@@ -20,7 +20,7 @@ func Init(cfg string) error {
 		return err
 	}
 
-    c.initLog()
+	c.initLog()
 
 	c.watchConfig()
 
@@ -48,28 +48,20 @@ func (c *Config) initConfig() error {
 func (c *Config) watchConfig() {
 	viper.WatchConfig()
 	viper.OnConfigChange(func(e fsnotify.Event) {
-        log.Infof("Config file changed %s", e.Name)
+		log.Infof("Config file changed %s", e.Name)
 	})
 }
 
 func (c *Config) initLog() {
-    passLagerCfg := log.PassLagerCfg {
-        Writers: viper.GetString("log.writers"),
-        LoggerLevel: viper.GetString("log.logger_level"),
-        LoggerFile: viper.GetString("log.logger_file"),
-        LogFormatText: viper.GetBool("log.log_format_text"),
-        RollingPolicy: viper.GetString("log.rollingPloify"),
-        LogRotateDate: viper.GetInt("log.log_rotate_date"),
-        LogRotateSize: viper.GetInt("log.log_rotate.size"),
-        LogBackupCount: viper.GetInt("log.log_backup_count"),
-    }
-    log.InitWithConfig(&passLagerCfg)
+	passLagerCfg := log.PassLagerCfg{
+		Writers:        viper.GetString("log.writers"),
+		LoggerLevel:    viper.GetString("log.logger_level"),
+		LoggerFile:     viper.GetString("log.logger_file"),
+		LogFormatText:  viper.GetBool("log.log_format_text"),
+		RollingPolicy:  viper.GetString("log.rollingPloify"),
+		LogRotateDate:  viper.GetInt("log.log_rotate_date"),
+		LogRotateSize:  viper.GetInt("log.log_rotate.size"),
+		LogBackupCount: viper.GetInt("log.log_backup_count"),
+	}
+	log.InitWithConfig(&passLagerCfg)
 }
-
-
-
-
-
-
-
-
--- main.go
+++ main.go
@@ -1,21 +1,23 @@
 package main
 
 import (
-    "net/http"
-    "github.com/lexkong/log"
-    "github.com/bigbignerd/GoRESTful/router"
-    "github.com/bigbignerd/GoRESTful/config"
+	"github.com/bigbignerd/GoRESTful/config"
+	"github.com/bigbignerd/GoRESTful/model"
+	"github.com/bigbignerd/GoRESTful/router"
+	"github.com/lexkong/log"
+	"net/http"
 
-    "github.com/gin-gonic/gin"
-    "github.com/spf13/pflag"
-    "github.com/spf13/viper"
+	"github.com/gin-gonic/gin"
+	"github.com/spf13/pflag"
+	"github.com/spf13/viper"
 )
 
 var (
-    cfg = pflag.StringP("config", "c", "", "apisever cofig file path")
+	cfg = pflag.StringP("config", "c", "", "apisever cofig file path")
 )
-func main(){
-    pflag.Parse()
+
+func main() {
+	pflag.Parse()
 
 	// init config
 	if err := config.Init(*cfg); err != nil {
@@ -25,13 +27,17 @@ func main(){
 	// Set gin mode.
 	gin.SetMode(viper.GetString("runmode"))
 
-    g := gin.New()
-    middlewares := []gin.HandlerFunc{}
+	g := gin.New()
+	middlewares := []gin.HandlerFunc{}
 
-    router.Load(
-        g,
-        middlewares...,
-    )
-    log.Infof("start to listening the incoming requests on http address %s",viper.GetString("addr")) 
-    log.Infof(http.ListenAndServe(viper.GetString("addr"), g).Error())
+	router.Load(
+		g,
+		middlewares...,
+	)
+	//init db
+	model.DB.Init()
+	defer model.DB.Close()
+
+	log.Infof("start to listening the incoming requests on http address %s", viper.GetString("addr"))
+	log.Infof(http.ListenAndServe(viper.GetString("addr"), g).Error())
 }
